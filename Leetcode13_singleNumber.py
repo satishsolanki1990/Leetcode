8b931cd6e2bb9514d5b9eb4089530e0c8dabd06e
satishsolanki1990 <satishsolanki1990@gmail.com>
2020-04-02 22:57:28 -0700

    find single number:used XOR

---
+++ Leetcode13_singleNumber.py
@@ -0,0 +1,30 @@
+'''
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+
+'''
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # XOR a number even number of times with itself, the result is 0.
+        x=0
+        for i in nums:
+            x ^= i
+        
+        return x
+        
