1cfc5f29c41e1724b320543c6d0cc1b73924da63
satishsolanki <satishsolanki1990@gmail.com>
2021-03-07 23:48:58 -0800

    update LC 1

--- 1_TwoSum.py
+++ 1_TwoSum.py
@@ -16,56 +16,27 @@ Output: [0,1]
 Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 """
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        """
-        # method :1, Brute force method
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if (nums[i]+nums[j])==target:
-                    return [i,j]
-        """
-# method :2, divide and conqure
-        if len(nums)==2:
-            return [0,1]
-        
-        try:
-            l=[i for i in range(len(nums)) if nums[i] == (target/2) ]
-            print(l)
-            if len(l)==2:
-                return l
-        except ValueError:
-            print('continue')        
-                
-        nums0=sorted(nums)
-        l=[]
-        flag1=len(nums)
-        if nums0[0]> 0:
-            for i in range(len(nums0)):
-                if nums0[i]>=target:
-                    flag1=i
-                    break        
-        # take the list only till flag1
-        nums1=nums0[0:flag1]
-        # make flag2 as nums1[] < target
-        
-        for i in range(len(nums1)):
-            if nums1[i]>target/2:
-                flag2=i
-                break
-        # Two for loops
-        for i in range(flag2):
-            for j in range(flag2,flag1):
-                if nums1[i]+nums1[j]==target:
-                    return [nums.index(nums1[i]),nums.index(nums1[j])]
-                    break                    
-        return l
-    
-    # find index for a number < (target/2)
-    # use that as flag1 and split the list
-    # if nums has all positive numbers the maximum index it will go till target
+from collections import defaultdict
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+        logic: for nums[i], if target-nums[i] in nums-> return indexes
+
+        Since we need frequent search, we can use hash map
+        '''
+
+        hashmap = defaultdict(list)
+
+        for i, v in enumerate(nums):
+            hashmap[v].append(i)
+
+        for k in hashmap:
+            temp = target - k
+            if temp in hashmap:
+                if k != temp:
+                    return [hashmap[k][0], hashmap[temp][0]]  # since there is unique element
+                else:
+                    if len(hashmap[k]) == 2:
+                        return hashmap[k]
