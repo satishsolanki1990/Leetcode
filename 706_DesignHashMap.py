2d5d7dfe4516eea7ab46b47a73248f2a23a436a9
satishsolanki <satishsolanki1990@gmail.com>
2021-03-28 14:06:31 -0700

    LC 706

---
+++ 706_DesignHashMap.py
@@ -0,0 +1,60 @@
+'''
+Design a HashMap without using any built-in hash table libraries.
+
+Implement the MyHashMap class:
+
+MyHashMap() initializes the object with an empty map.
+void put(int key, int value) inserts a (key, value) pair into the HashMap.
+If the key already exists in the map, update the corresponding value.
+int get(int key) returns the value to which the specified key is mapped,
+ or -1 if this map contains no mapping for the key.
+void remove(key) removes the key and its corresponding value
+if the map contains the mapping for the key.
+
+
+Example 1:
+
+Input
+["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+[[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+Output
+[null, null, null, 1, -1, null, 1, null, -1]
+
+'''
+
+
+class MyHashMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.hashmap = {}
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+        self.hashmap[key] = value
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+        if key in self.hashmap:
+            return self.hashmap[key]
+        else:
+            return -1
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+        if key in self.hashmap:
+            del self.hashmap[key]
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
