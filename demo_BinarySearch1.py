a27387319165687fba900f8310f2f8b9ba94e6cc
satishsolanki <satishsolanki1990@gmail.com>
2021-03-28 13:57:45 -0700

    update demo_BinarySearch1.py

--- demo_BinarySearch1.py
+++ demo_BinarySearch1.py
@@ -7,5 +7,22 @@ for ex: search a number from sorted list
 '''
 def binarySearch(arr,target):
     l=0
-    r=len(arr)-1
-    while()
+    r=len(arr)
+    while(l<=r):
+        mid = l+(r-l)//2
+        if arr[mid] == target:
+            print(f'target {target} found at index {mid} or position {mid+1}  in array.')
+            return True
+
+        if arr[mid]>target:
+            r=mid-1
+        else:
+            l = mid +1
+
+    print(f'target {target} not found in array.')
+    return False
+
+a=[1,2,4,5,6,8]
+binarySearch(a,1)
+
+
